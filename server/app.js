ae19052700792a3eb3f6ea6338a5314a011decfe
bayu <bayuferdiman1@gmail.com>
2024-08-23 15:44:39 +0700

    set sequelize

---
+++ server/app.js
@@ -0,0 +1,12 @@
+require("dotenv").config({ path: `${process.cwd()}/.env` });
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
