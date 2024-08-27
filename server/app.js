c36de784004c21c6e5f2fd91939f035b65d5489b
bayu <bayuferdiman1@gmail.com>
2024-08-27 15:08:42 +0700

    cannot migrate

--- server/app.js
+++ server/app.js
@@ -1,12 +1,25 @@
 require("dotenv").config({ path: `${process.cwd()}/.env` });
 const express = require("express");
+const cors = require('cors')
+const morgan = require("morgan");
 const app = express();
 
+let corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions)) // agar bisa diakses oleh semua domain
+app.use(morgan("dev")); // log request ke console
+app.use(express.json()); // agar bisa membaca json
+app.use(express.urlencoded({ extended: true })); // agar bisa membaca data dari form
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({
+    "message": "Welcome to my nakao API"
+  });
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
