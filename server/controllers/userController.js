50dd55f9774120050d0db553ec0a9d6c43e19c00
bayu <bayuferdiman1@gmail.com>
2024-09-06 09:46:32 +0700

    be: delete unused code

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const User = require("../db/models/user"); // Ubah nama variabel ini
+const User = require("../db/models/user"); 
 
 const generateToken = (payload) => {
   return jwt.sign({ payload }, process.env.JWT_SECRET_KEY, {
@@ -15,12 +15,12 @@ const signup = async (req, res) => {
 
   try {
     // validation apabila email sudah terdaftar
-    const isEmailExist = await User.findOne({ where: { email: body.email } }); // Ubah nama variabel ini
+    const isEmailExist = await User.findOne({ where: { email: body.email } }); 
     if (isEmailExist) {
       return res.status(400).json({ message: "Email already exist" });
     }
     
-    const newUser = await User.create({ // Ubah nama variabel ini
+    const newUser = await User.create({ 
       name: body.name,
       email: body.email,
       password: bcrypt.hashSync(body.password, 10),
@@ -28,9 +28,9 @@ const signup = async (req, res) => {
     });
 
     const token = generateToken({
-      id: newUser.id, // Ubah nama variabel ini
-      email: newUser.email, // Ubah nama variabel ini
-      role: newUser.role, // Ubah nama variabel ini
+      id: newUser.id, 
+      email: newUser.email, 
+      role: newUser.role, 
     });
     return res.status(201).json({
       message: "User created successfully",
@@ -47,20 +47,20 @@ const login = async (req, res) => {
   const body = req.body;
 
   try {
-    const user = await User.findOne({ where: { email: body.email } }); // Ubah nama variabel ini
+    const user = await User.findOne({ where: { email: body.email } }); 
     if (!user) {
       return res.status(401).json({ message: "Invalid email or password" });
     }
 
-    const isPasswordMatch = bcrypt.compareSync(body.password, user.password); // Ubah nama variabel ini
+    const isPasswordMatch = bcrypt.compareSync(body.password, user.password); 
     if (!isPasswordMatch) {
       return res.status(401).json({ message: "Invalid email or password" });
     }
 
     const token = generateToken({
-      id: user.id, // Ubah nama variabel ini
-      email: user.email, // Ubah nama variabel ini
-      role: user.role, // Ubah nama variabel ini
+      id: user.id, 
+      email: user.email, 
+      role: user.role, 
     });
     return res.status(200).json({ token: token });
   } catch (error) {
